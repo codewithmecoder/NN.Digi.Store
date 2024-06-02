421f2e4eb32086a81494e42e990fd56aa9d2b2c6
codewithmecoder <yaty1031@gmail.com>
2024-06-02 23:01:40 +0700

    add user dto

---
+++ src/server/NN.Digi.Store/Application/Dto/Users/CreateUserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace NN.Digi.Store.Application.Dto.Users;
+
+public class CreateUserDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
