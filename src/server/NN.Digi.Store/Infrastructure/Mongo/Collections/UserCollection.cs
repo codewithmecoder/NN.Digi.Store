0093eaf30bf3dc99cf6f566af7bdf16148b17bc2
codewithmecoder <yaty1031@gmail.com>
2024-06-02 23:02:01 +0700

    add mongo

---
+++ src/server/NN.Digi.Store/Infrastructure/Mongo/Collections/UserCollection.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NN.Digi.Store.Infrastructure.Mongo.Collections;
+
+public class UserCollection
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string? Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ src/server/NN.Digi.Store/Infrastructure/Mongo/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using NN.Digi.Store.Domain;
+
+namespace NN.Digi.Store.Infrastructure.Mongo;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMongo(this IServiceCollection services, ApplicationSetting setting)
+    {
+        var mongoSetting = setting.MongoConnection;
+        string url;
+        if (string.IsNullOrEmpty(setting.MongoConnection.UserName) || string.IsNullOrEmpty(setting.MongoConnection.Password))
+        {
+            //mongodb://localhost:27017
+            url = $"mongodb://{mongoSetting.Host}:{mongoSetting.Port}";
+        }
+        else
+        {
+            url = $"mongodb://{mongoSetting.UserName}:{mongoSetting.Password}{mongoSetting.Host}:{mongoSetting.Port}";
+        }
+
+        var mongoClient = new MongoClient(url);
+        var mongoDatabase = mongoClient.GetDatabase(mongoSetting.DatabaseName);
+        services.AddScoped(provider => mongoDatabase);
+        return services;
+    }
+}
